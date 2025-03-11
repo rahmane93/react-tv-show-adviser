29505c962b0b6c7940f5b1f7d73b9ad47ea4d9b5
Rahmane BAH <rahmane.bah@gmail.com>
2025-03-11 23:38:47 +0100

    update index and config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,4 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
-<<<<<<< HEAD
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
 const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300/";
 export { BACKDROP_BASE_URL, BASE_URL, SMALL_IMG_COVER_BASE_URL };
-=======
-const API_KEY =
-  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMWY5NDk0ZTgwZGJiM2M5ODYxOGRiMzA5MzA0YWM3MiIsIm5iZiI6MTc0MTUzNDE4My43NDgsInN1YiI6IjY3Y2RiM2U3NzVjOWYxYmQxMmUyZDMzMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xsCjiuqzzX3LYHSH7M1go06Ja4IA5tSzgAdEyttzRwY";
-const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
-const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300/";
-export { API_KEY, BACKDROP_BASE_URL, BASE_URL, SMALL_IMG_COVER_BASE_URL };
->>>>>>> 6c400b503d77a4cad3d81b7bb7dd136389bad607
